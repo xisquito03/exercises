ba8b5c603479876ce49464005ba0f99fcdc7e935
xisquito03 <xisco034@gmail.com>
2022-11-15 11:22:44 +0100

    5

--- Assets/Scripts/exercise_05.cs
+++ Assets/Scripts/exercise_05.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class exercise_05 : MonoBehaviour
 {
-    [SerializeField] private int x;
-    [SerializeField] private int y;
+    [SerializeField] public int x=5;
+    [SerializeField] public int y=6;
+    private bool isGreater;
 
-   
+    private void Start()
+    {
+        isGreater = x > y;
+        Debug.Log(isGreater);
+    }
+
+
 }
